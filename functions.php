2d9f879d656476b91cf3c6a3a8709c480b18ac22
LairBob <laurent@stanevich.com>
2010-05-19 19:03:04 -0400

    Just upgraded to 3.0b2 -- about to start making specific templates for "Essays", "Sections", etc.
    Signed-off-by: LairBob <laurent@stanevich.com>

--- functions.php
+++ functions.php
@@ -7,3 +7,15 @@ function my_theme_init() {
 }
 
 add_action('init', 'my_theme_init');
+add_theme_support( 'nav-menus' );
+
+add_filter('mce_css', 'my_editor_style');
+
+function my_editor_style($url) {
+  if ( !empty($url) )
+    $url .= ',';
+  // Change the path here if using sub-directory
+  $url .= trailingslashit( get_stylesheet_directory_uri() ) . 'style.css';
+
+  return $url;
+}
